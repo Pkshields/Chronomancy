e04c57ea73ecfa81ef9e9ad2601e539bf076748b
Paul Shields <pkshields@gmail.com>
2021-01-28 14:09:39 +0000

    Adding ktlint and detekt to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
-plugins {
-    kotlin("jvm") version "1.4.21"
-}
-
 group = "dev.paulshields.chronomancy"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+}
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+
+    id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
+    id("org.jlleitschuh.gradle.ktlint").version("9.4.1")
 }
 
 dependencies {
